180dbf1f748b8bffea53f6391d8da33934fb962b
ccl1007 <luku612150@gmail.com>
2022-05-10 12:30:51 +0800

    fix

--- all.js
+++ all.js
@@ -1,15 +1,8 @@
 let map = L.map("map", {
-  center: [24.173188212752365, 120.67684804724759],
+  center: [24.16586300126418, 120.6365095478168],
   zoom: 20,
 });
 
-let Icon = new L.Icon({
-  iconUrl: "https://gosaico.com/images/svg/logo.svg",
-  iconSize: [40, 40],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
 
 //加入圖層
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -18,22 +11,16 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 //定義座標
-let greenIcon = new L.Icon({
-  iconUrl:
-    "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+let Icon = new L.Icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/822/822586.png",
   shadowUrl:
     "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-  iconSize: [25, 41],
+  iconSize: [30, 30],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41],
 });
 
-// 加入奇點無限座標
-L.marker([24.173188212752365, 120.67684804724759], { icon: Icon })
-  .addTo(map)
-  .bindPopup("奇點無限")
-  .openPopup();
 
 //新增Clustert插件圖層來放icon群組
 let markers = new L.MarkerClusterGroup().addTo(map);
@@ -42,8 +29,7 @@ let markers = new L.MarkerClusterGroup().addTo(map);
 let data = [];
 
 axios
-  .get("https://raw.githubusercontent.com/SiongSng/Rapid-Antigen-Test-Taiwan-Map/data/data/antigen_open_street_map.json"
-  )
+  .get("https://kiang.github.io/data.nhi.gov.tw/antigen.json")
   .then((res) => {
     data = res.data.features;
     console.log(data);
@@ -51,17 +37,20 @@ axios
       markers.addLayer(
         L.marker(
           [data[i].geometry.coordinates[1], data[i].geometry.coordinates[0]],
-          { icon: greenIcon }
+          { icon: Icon }
         )
           .bindPopup(`<p><strong style="font-size: 20px;">${data[i].properties.name}</strong></p>
-       <span>品牌: ${data[i].properties.brands}</br>
+       <span>品牌: ${data[i].properties.brand}</br>
       <strong style="font-size: 16px;">剩餘 
       ${data[i].properties.count} 份 (每份五個)
       </strong><br>
       地址: ${data[i].properties.address}<br>
       電話: ${data[i].properties.phone}<br>
       備註: ${data[i].properties.note}<br>
-      <small>最後更新時間: ${data[i].properties.updated_at}</small></span>`)
+      <small>最後更新時間: ${data[i].properties.updated}</small></span>`)
       );
     }
+  })
+  .catch((error) => {
+    alert(error.message);
   });
